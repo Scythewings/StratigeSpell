402d2ac844f63e5c7453d248df08b00499d241d2
zwerchhau <139905891+zwerchhau@users.noreply.github.com>
2023-07-19 15:08:14 +0700

    Camera script

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -10,6 +10,8 @@ public class Movement : MonoBehaviour
     [SerializeField] private LayerMask _border;
     public BaseChar basec;
     public int action;
+
+    [SerializeField] private CamController CameraScript;
         
     // Start is called before the first frame update
     void Start()
