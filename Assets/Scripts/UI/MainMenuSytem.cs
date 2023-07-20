c908443aa50f62bbd08eee1404e900c3bfbecd2c
Kvaralin <annxxiii23@gmail.com>
2023-07-20 12:17:54 +0700

    Moving Main Menu

--- Assets/Scripts/UI/MainMenuSytem.cs
+++ Assets/Scripts/UI/MainMenuSytem.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuSytem : MonoBehaviour
 {
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
+    }
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void GamePlay()
+    {
+        SceneManager.LoadScene("GamePlay", LoadSceneMode.Single);
     }
 }
