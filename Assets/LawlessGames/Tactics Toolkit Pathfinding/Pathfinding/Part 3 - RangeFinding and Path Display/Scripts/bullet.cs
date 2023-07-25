dc08b89e317b84e1d991baefaa87225316887c36
Scythewings <putawanswimming13@gmail.com>
2023-07-25 10:55:24 +0700

    Projectile Doesn't collide with Map

--- Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/bullet.cs
+++ Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/bullet.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-   
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Physics2D.IgnoreLayerCollision(0, 7);
+
         if (collision.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent))
         {
             enemyComponent.takeDamage(1);
