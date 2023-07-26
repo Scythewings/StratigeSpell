ac1f3a946d5e726699ebbf19e0cf428a3d79f1da
Temuzuzu <97076790+Temuzuzu@users.noreply.github.com>
2023-07-26 21:38:03 +0700

    CharacterDetail

--- Assets/Scripts/CheckTeam.cs
+++ Assets/Scripts/CheckTeam.cs
@@ -1,3 +1,4 @@
+using finished3;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,7 +12,7 @@ public class CheckTeam : MonoBehaviour
     {
         foreach (GameObject v in Resources.FindObjectsOfTypeAll(typeof(GameObject)))
         {
-            if (v.TryGetComponent<>(out CharacterDetail))
+            if (v.TryGetComponent<CharacterDetail>(out CharacterDetail))
             {
 
             }
