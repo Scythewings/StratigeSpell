8924987f6f86d450bec6ab766e667f40afedb4ff
Scythewings <putawanswimming13@gmail.com>
2023-07-28 20:56:32 +0700

    Check Team

--- Assets/Scripts/CheckTeam.cs
+++ Assets/Scripts/CheckTeam.cs
@@ -2,6 +2,7 @@ using finished3;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckTeam : MonoBehaviour
 {
@@ -17,11 +18,11 @@ public class CheckTeam : MonoBehaviour
     {
         if (redAlive == 0)
         {
-            //end blue win
+            SceneManager.LoadScene("MainMenu");
         }
         else if (blueAlive == 0)
         {
-            //end red win
+            SceneManager.LoadScene("MainMenu");
         }
     }
 }
